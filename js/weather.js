ffbe7d5b7c91c51f7dd62658f9af245c355bf9ee
Robert Woodruff <rwoodruff@Roberts-Air.fios-router.home>
2021-01-31 23:58:46 -0500

    submission with part 2

--- js/weather.js
+++ js/weather.js
@@ -1,53 +1,48 @@
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PART ONE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+let lon = 0.0;
+let lat = 0.0;
+let temp = document.getElementById('weather');
+let loc = document.getElementById('zipCode');
 
-//Step 1. Creates an XMLHttp Request Object
-let xhr = new XMLHttpRequest();
-let xhr2 = new XMLHttpRequest();
-
-const zipCode = 19143; //prompt("Enter zip code to get City, State, and Weather?");
-let lon;
-let lat;
-
-//Step 2. Create a callback function (Asyncronous)
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        let locationResponse = JSON.parse(xhr.responseText);
-        lon = locationResponse.places[0].longitude;
-        lat = locationResponse.places[0].latitude;
-        let city = locationResponse.places[0]["place name"];
-        let state = locationResponse.places[0]["state abbreviation"];
-        console.log(locationResponse);
-        console.log(lon);
-        console.log(lat);
-        document.getElementById('zipCode').innerHTML = city + ", " + state;
-    } else if (xhr.status === 404) {
-         alert = "404 error, Response Not Found";
+function getZipCode() {
+    const zipCode = prompt("Enter zip code to get City, State, and Weather?");
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let locationResponse = JSON.parse(xhr.responseText);
+            lon = locationResponse.places[0].longitude;
+            lat = locationResponse.places[0].latitude;
+            const city = locationResponse.places[0]["place name"];
+            const state = locationResponse.places[0]["state abbreviation"];
+            // console.log(locationResponse);
+            // console.log(lon);
+            // console.log(lat);
+            getWeather(lon, lat);
+            const imgBkgrd = `<img src="" id="forcast">`;
+            loc.innerHTML = city + ", " + state;
+        // } else if (xhr.status === 404) {
+        //     alert = "404 error, Response Not Found";
+        }     
     }
-}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PART TWO~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-xhr2.onreadystatechange = function() {
-    if (xhr2.readyState === 4 && xhr2.status === 200) {
-        let weatherResponse = JSON.parse(xhr2.responseText);
-        let temp = weatherResponse.dataseries[0].weather;
-        console.log(weatherResponse);
-        document.getElementById('weather').innerHTML = temp;
-    } 
-}  
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-//Step 3. Open request
-
-xhr.open('GET', 'http://api.zippopotam.us/US/'+zipCode);
-
-//Below code returns the incorrect weather type of lightrain when passing the values via declared variables
-//xhr2.open('GET', `http://www.7timer.info/bin/api.pl?product=civillight&lon=${lon}&lat=${lat}&output=json`);
-
-//If hard coded, correct weather type is returned
-xhr2.open('GET', 'http://www.7timer.info/bin/api.pl?product=civillight&lon=-75.2288&lat=39.9448&output=json');
-
-//Step 4. Send request
-function sendAJAX() {
+    xhr.open('GET', `http://api.zippopotam.us/US/${zipCode}`);
     xhr.send();
+}
+    let xhr2 = new XMLHttpRequest();                
+    xhr2.onreadystatechange = function() { 
+        if (xhr2.readyState === 4 && xhr2.status === 200) {
+            let weatherResponse = JSON.parse(xhr2.responseText);
+            //let forcast = weatherResponse.dataseries[0].weather;
+            min = weatherResponse['dataseries'][0]['temp2m'].min * 9 / 5 + 32;
+            max = weatherResponse['dataseries'][0]['temp2m'].max * 9 / 5 + 32;
+            //let tempAvg = (min + max) / 2;
+            console.log(weatherResponse);
+            //.innerHTML = Math.floor(max);
+            temp.innerHTML = Math.floor(max) + `&degF`.sup();
+
+            //document.getElementById('weather').innerHTML = Math.floor(max)+`&degF`.sup();
+        } 
+    }
+
+function getWeather(lon, lat) {
+    xhr2.open('GET', `http://www.7timer.info/bin/api.pl?product=civillight&lon=${lon}&lat=${lat}&output=json`);
     xhr2.send();
-  }
+}
